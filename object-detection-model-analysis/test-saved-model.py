1898eb2ce456ad12406c574259b0f55514e2cb9d
Simon <simonmyhre1@gmail.com>
2023-01-30 21:27:48 +0100

    feat: see result of inference graph model

---
+++ object-detection-model-analysis/test-saved-model.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from six import BytesIO
+import cv2
+
+saved_model_path = '../saved_models/inference_graph/saved_model'
+def load_image_into_numpy_array(path):
+  """Load an image from file into a numpy array.
+
+  Puts image into numpy array to feed into tensorflow graph.
+  Note that by convention we put it into a numpy array with shape
+  (height, width, channels), where channels=3 for RGB.
+
+  Args:
+    path: a file path (this can be local or on colossus)
+
+  Returns:
+    uint8 numpy array with shape (img_height, img_width, 3)
+  """
+  img_data = tf.io.gfile.GFile(path, 'rb').read()
+  image = Image.open(BytesIO(img_data))
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+def reframe_image_corners(boxes):
+    ymin, xmin, ymax, xmax = (boxes[:, 0], boxes[:, 1], boxes[:, 2], boxes[:, 3])
+    height = tf.maximum(ymax - ymin, 1e-4)
+    width = tf.maximum(xmax - xmin, 1e-4)
+    ymin_out = (0 - ymin) / height
+    xmin_out = (0 - xmin) / width
+    ymax_out = (1 - ymin) / height
+    xmax_out = (1 - xmin) / width
+    return tf.stack([ymin_out, xmin_out, ymax_out, xmax_out], axis=1)
+def reframe_box_masks_to_image_masks(box_masks, boxes, image_height, image_width):
+    if tf.shape(box_masks)[0] > 0:
+        num_boxes = tf.shape(box_masks)[0]
+        box_masks_expanded = tf.expand_dims(box_masks, axis=3)
+        resized_crops = tf.image.crop_and_resize(
+            image=box_masks_expanded,
+            boxes=reframe_image_corners(boxes),
+            box_ind=tf.range(num_boxes),
+            crop_size=[image_height, image_width],
+            extrapolation_value=0)
+        return tf.cast(resized_crops, box_masks.dtype)
+    else:
+        return tf.zeros([0, image_height, image_width, 1], box_masks.dtype)
+def run_inference_for_single_image(model, image):
+    image = np.asarray(image)
+    input_tensor = tf.convert_to_tensor(image)
+    input_tensor = input_tensor[tf.newaxis, ...]
+
+    model_fn = model.signatures['serving_default']
+    output_dict = model_fn(input_tensor)
+
+    num_detections = int(output_dict.pop('num_detections'))
+    output_dict = {key: value[0, :num_detections].numpy()
+                   for key, value in output_dict.items()}
+
+    output_dict['num_detections'] = num_detections
+    output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
+
+    if 'detection_masks' in output_dict:
+        detection_masks_reframed = reframe_box_masks_to_image_masks(
+            output_dict['detection_masks'], output_dict['detection_boxes'],
+            image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(detection_masks_reframed > 0.2, tf.uint8)
+        output_dict['detection_masks_reframed'] = detection_masks_reframed.numpy()
+
+    return output_dict
+
+# Load the model
+model = tf.saved_model.load(saved_model_path)
+
+# Load image
+image_np = load_image_into_numpy_array('../data/dataset-test-words/1.jpg')
+output_dict = run_inference_for_single_image(model, image_np)
+
+detection_boxes = output_dict['detection_boxes']
+detection_scores = output_dict['detection_scores']
+
+# Cut the bounding boxes out of the image
+def cut_boxes_from_image(image, boxes, scores):
+    images = []
+    for box, score in zip(boxes, scores):
+        if score < 0.2:
+            continue
+        xmin, ymin, xmax, ymax = box
+        xmin = int(xmin * image.shape[1])
+        ymin = int(ymin * image.shape[0])
+        xmax = int(xmax * image.shape[1])
+        ymax = int(ymax * image.shape[0])
+        images.append(image[xmin:xmax, ymin:ymax])
+    return images
+images = cut_boxes_from_image(image_np, detection_boxes, detection_scores)
+
+"""
+# This creates an image with the bounding boxes drawn on it
+def draw_boxes_on_image(image, boxes, scores, line_width=2):
+    """"""Draw bounding boxes on an image.
+
+    Args:
+    - image: numpy array representing the image
+    - boxes: list of bounding box coordinates, in the format of [[xmin, ymin, xmax, ymax]]
+    - color: tuple representing the color of the boxes in (B, G, R) format
+    - line_width: integer representing the width of the lines
+
+    Returns:
+    - image: numpy array representing the image with the bounding boxes drawn on it
+    """"""
+    for box, score in zip(boxes, scores):
+        if score < 0.2:
+            continue
+        xmin, ymin, xmax, ymax = box
+        xmin = int(xmin * image.shape[1])
+        ymin = int(ymin * image.shape[0])
+        xmax = int(xmax * image.shape[1])
+        ymax = int(ymax * image.shape[0])
+        cv2.rectangle(image, (ymin, xmin), (ymax, xmax), (0, 255, 0), line_width)
+    return image
+
+new_image = draw_boxes_on_image(image_np, detection_boxes, detection_scores)
+plt.imshow(new_image)
+plt.show()
+"""
