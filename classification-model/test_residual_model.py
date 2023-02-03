3658718b071c96d870e15eb80a71fe1ce3c0852d
Simon Myhre <simonmyhre1@gmail.com>
2023-02-03 14:57:49 +0100

    feat: format residual block into a class

--- classification-model/test_residual_model.py
+++ classification-model/test_residual_model.py
@@ -3,8 +3,9 @@ import tensorflow as tf
 def create_test_residual_model(number_output_layers):
     inputs = tf.keras.Input(shape=(50, 250, 1))
     conv1 = tf.keras.layers.Conv2D(64, (3,3), activation='relu')(inputs)
-    resblock1 = residual_block(conv1, 32, (3,3))
-    pool1 = tf.keras.layers.MaxPooling2D(2, 2)(resblock1)
+    resblock1 = ResidualBlock(32, (3,3))(conv1)
+    resblock2 = ResidualBlock(32, (3,3))(resblock1)
+    pool1 = tf.keras.layers.MaxPooling2D(2, 2)(resblock2)
     pool2 = tf.keras.layers.MaxPooling2D(2, 2)(pool1)
     flatten = tf.keras.layers.Flatten()(pool2)
     dense1 = tf.keras.layers.Dense(128, activation='relu')(flatten)
@@ -13,10 +14,26 @@ def create_test_residual_model(number_output_layers):
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     return model
 
-def residual_block(inputs, filters, kernel_size):
-    x = tf.keras.layers.Conv2D(filters, kernel_size, padding='same', strides=1)(inputs)
-    x = tf.keras.layers.ReLU()(x)
-    x = tf.keras.layers.Conv2D(filters, kernel_size, padding='same')(x)
-    shortcut = tf.keras.layers.Conv2D(filters, (1, 1), padding='same', strides=1)(inputs)
-    x = tf.keras.layers.Add()([x, shortcut])
-    return x
+
+class ResidualBlock(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, **kwargs):
+        super(ResidualBlock, self).__init__(**kwargs)
+        self.filters = filters
+        self.kernel_size = kernel_size
+
+    def build(self, input_shape):
+        self.conv1 = tf.keras.layers.Conv2D(self.filters, self.kernel_size, padding='same', strides=1)
+        self.relu = tf.keras.layers.ReLU()
+        self.conv2 = tf.keras.layers.Conv2D(self.filters, self.kernel_size, padding='same')
+        self.conv3 = tf.keras.layers.Conv2D(self.filters, (1, 1), padding='same', strides=1)
+        self.add = tf.keras.layers.Add()
+        super(ResidualBlock, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        x = self.conv1(inputs)
+        x = self.relu(x)
+        x = self.conv2(x)
+        shortcut = self.conv3(inputs)
+        x = self.add([x, shortcut])
+        return x
+
