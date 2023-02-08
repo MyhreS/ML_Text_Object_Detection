1142097df06b094fe9e54e53303c6f66cbc0b762
Simon <simonmyhre1@gmail.com>
2023-02-08 12:20:35 +0100

    feat: shorten model

--- classification-model/residual_model_1.py
+++ classification-model/residual_model_1.py
@@ -2,23 +2,26 @@ import tensorflow as tf
 from residual_block import ResidualBlock
 def create_residual_model_1(number_output_layers):
     def create_lane(resblock1, lane_number):
-        conv = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(resblock1)
-        batchnorm = tf.keras.layers.BatchNormalization()(conv)
-        resblock = ResidualBlock(32, (3, 3))(batchnorm)
-        pool = tf.keras.layers.MaxPooling2D(2, 2)(resblock)
-        resblock = ResidualBlock(32, (3, 3))(pool)
-        pool = tf.keras.layers.MaxPooling2D(2, 2)(resblock)
-        conv = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(pool)
-        flatten = tf.keras.layers.Flatten()(conv)
-        dense = tf.keras.layers.Dense(128, activation='relu')(flatten)
-        dense_output = tf.keras.layers.Dense(27, activation='softmax', name=f'output{lane_number}')(dense)
+        conv_x_1 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(resblock1)
+        batchnorm_x_1 = tf.keras.layers.BatchNormalization()(conv_x_1)
+        resblock_x_1 = ResidualBlock(32, (3, 3))(batchnorm_x_1)
+        pool_x_1 = tf.keras.layers.MaxPooling2D(2, 2)(resblock_x_1)
+        resblock_x_2 = ResidualBlock(32, (3, 3))(pool_x_1)
+        pool_x_2 = tf.keras.layers.MaxPooling2D(2, 2)(resblock_x_2)
+        conv_x_2 = tf.keras.layers.Conv2D(64, (3, 3), activation='relu')(pool_x_2)
+        flatten_x = tf.keras.layers.Flatten()(conv_x_2)
+        dense_x_1 = tf.keras.layers.Dense(128, activation='relu')(flatten_x)
+        dense_output = tf.keras.layers.Dense(27, activation='softmax', name=f'output{lane_number}')(dense_x_1)
         return dense_output
 
     # Feature extraction
     inputs = tf.keras.Input(shape=(50, 250, 1))
     conv1 = tf.keras.layers.Conv2D(64, (3,3), activation='relu')(inputs)
-    batchnorm1 = tf.keras.layers.BatchNormalization()(conv1)
+    pool1 = tf.keras.layers.MaxPooling2D(2, 2)(conv1)
+    batchnorm1 = tf.keras.layers.BatchNormalization()(pool1)
     resblock1 = ResidualBlock(32, (3,3))(batchnorm1)
+
+
 
     outputs = []
     for i in range(1, number_output_layers+1):
