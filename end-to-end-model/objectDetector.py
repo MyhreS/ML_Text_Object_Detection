66783e5047728e1c1e26b17c60b620334a86863d
Simon <simonmyhre1@gmail.com>
2023-02-08 16:13:23 +0100

    feat: remove object detector

--- end-to-end-model/objectDetector.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-class ScaleLayer(tf.keras.layers.Layer):
-  def __init__(self, scale, **kwargs):
-    super(ScaleLayer, self).__init__(**kwargs)
-    self.scale = scale
-
-  def call(self, inputs):
-    return inputs * self.scale
