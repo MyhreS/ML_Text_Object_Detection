d0b5ead2c809b43932b7e54e47d4814a2f5200d0
Simon <simonmyhre1@gmail.com>
2023-03-16 21:40:02 +0100

    feat: create script to invert images to make new dataset

---
+++ data-analysis/dataset-5-to-6.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageOps
+import glob
+
+"""
+This script inverts the colors of the images in the dataset-5-words-with-characters-bb folder to create the dataset-6-words-with-characters-bb folder.
+"""
+def invert_image(input_file, output_folder):
+    # Open the image file
+    img = Image.open(input_file)
+
+    # Invert the colors
+    inverted_img = ImageOps.invert(img)
+
+    # Save the inverted image to the output folder
+    output_file = output_folder + '/' + input_file.split('\\')[-1]
+    inverted_img.save(output_file)
+
+
+train_images = glob.glob("../data/dataset-5-words-with-characters-bb/train/*.jpg")
+test_images = glob.glob("../data/dataset-5-words-with-characters-bb/test/*.jpg")
+val_images = glob.glob("../data/dataset-5-words-with-characters-bb/valid/*.jpg")
+
+for train_image in train_images:
+    invert_image(train_image, "../data/dataset-6-words-with-characters-bb/train")
+
+for test_image in test_images:
+    invert_image(test_image, "../data/dataset-6-words-with-characters-bb/test")
+
+for val_image in val_images:
+    invert_image(val_image, "../data/dataset-6-words-with-characters-bb/valid")
+
