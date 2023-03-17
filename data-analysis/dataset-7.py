be1f67b8caec3065e0d5bdc1ab9391f2497fd001
Simon <simonmyhre1@gmail.com>
2023-03-17 13:27:37 +0100

    feat: create script to convert annotations

---
+++ data-analysis/dataset-7.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+"""
+This script converts the predictions.csv file to the annotations.csv file.
+"""
+
+# Load predictions.csv
+predictions = pd.read_csv("../data/dataset-7-raw-test-dataset-4-with-bb/predictions.csv")
+
+# Choose which columns to keep
+predictions = predictions[["x", "y", "width", "height", "class", "image_path"]]
+
+# Fix the image_path column
+predictions["image_path"] = predictions["image_path"].apply(lambda path: path.split("/")[-1])
+
+print(predictions.head())
+
+# Save as a csv
+predictions.to_csv("../data/dataset-7-raw-test-dataset-4-with-bb/annotations.csv", index=False)
