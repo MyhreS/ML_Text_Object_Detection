5e407ca11e44cdb77ecc30cc2e1cd5592c0ab928
Simon <simonmyhre1@gmail.com>
2023-04-15 22:32:14 +0200

    fix: reorder structure of object detectors

--- models/object_detection/evaluate/test_by_cutting_words_out_from_form.py
+++ models/object_detection/evaluate/test_by_cutting_words_out_from_form.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import utils
 
 """
-This script tests the saved model by drawing bounding boxes around the detected words from a single form.
+This script tests the saved model by cutting out the first detected word from the form image.
 """
 
 saved_model_path = '../saved_models/faster-rcnn-training-v2-for-forms/inference_graph/saved_model'
@@ -11,16 +11,14 @@ saved_model_path = '../saved_models/faster-rcnn-training-v2-for-forms/inference_
 # Load the model
 model = tf.saved_model.load(saved_model_path)
 
-image_np = utils.load_image_into_numpy_array('../../datasets/dataset-test-words/1.jpg')
+# Load image
+image_np = utils.load_image_into_numpy_array('../../../datasets/dataset-test-words/1.jpg')
 output_dict = utils.run_inference_for_single_image(model, image_np)
 
 detection_boxes = output_dict['detection_boxes']
 detection_scores = output_dict['detection_scores']
 
-new_image = utils.draw_boxes_on_image(image_np, detection_boxes, detection_scores)
-plt.imshow(new_image)
+images = utils.cut_boxes_from_image(image_np, detection_boxes, detection_scores)
+
+plt.imshow(images[1])
 plt.show()
-
-
-
-
--- models/object_detection/evaluate/test_by_drawing_bounding_boxes_around_words_in_form.py
+++ models/object_detection/evaluate/test_by_drawing_bounding_boxes_around_words_in_form.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import utils
 
 """
-This script tests the saved model by cutting out the first detected word from the form image.
+This script tests the saved model by drawing bounding boxes around the detected words from a single form.
 """
 
 saved_model_path = '../saved_models/faster-rcnn-training-v2-for-forms/inference_graph/saved_model'
@@ -11,14 +11,16 @@ saved_model_path = '../saved_models/faster-rcnn-training-v2-for-forms/inference_
 # Load the model
 model = tf.saved_model.load(saved_model_path)
 
-# Load image
-image_np = utils.load_image_into_numpy_array('../../datasets/dataset-test-words/1.jpg')
+image_np = utils.load_image_into_numpy_array('../../../datasets/dataset-test-words/1.jpg')
 output_dict = utils.run_inference_for_single_image(model, image_np)
 
 detection_boxes = output_dict['detection_boxes']
 detection_scores = output_dict['detection_scores']
 
-images = utils.cut_boxes_from_image(image_np, detection_boxes, detection_scores)
-
-plt.imshow(images[1])
+new_image = utils.draw_boxes_on_image(image_np, detection_boxes, detection_scores)
+plt.imshow(new_image)
 plt.show()
+
+
+
+
